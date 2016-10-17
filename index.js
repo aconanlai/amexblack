03f1fd8ca79734d60b773fc1adb447673a6f2be9
conan <aconanlai@gmail.com>
2016-10-17 16:47:33 -0400

    add express server for localtunnel

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+ app.get('/', function(req, res) {
+    res.sendFile('index.html');
+ });
+
+ app.use(express.static('./'));
+
+ app.listen(5000, function() {
+   console.log('Listening on 5000');
+ });
+
